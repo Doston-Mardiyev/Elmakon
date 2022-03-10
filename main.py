cad9a772908a961d19aa719981733e1bb6d1e394
Doston Mardiyev <dmardiyev96@gmail.com>
2022-03-10 18:04:47 +0500

    commit

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from email import header
 from urllib import response
 import requests
 from bs4 import BeautifulSoup
+import time
+from random import randrange
 
 header = {
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
@@ -17,21 +18,52 @@ def get_article_urls(url):
 
     soup = BeautifulSoup(response.text, 'lxml')
     pagination_count = int(soup.find('div', class_='ty-pagination__items').find_all('a')[-1].text)
+    articles_urls_list = []
 
-    for page in range(1, 2):
+    for page in range(1, pagination_count + 1):
+    # for page in range(1, 2):
         response = s.get(url=f'https://elmakon.uz/telefony-gadzhety-aksessuary/smartfony/page-{page}/', headers=header)
-        #soup = BeautifulSoup(response.text, 'lxml')
+        soup = BeautifulSoup(response.text, 'lxml')
 
-        #articles_info = soup.find_all('a', class_='')
+        articles_info = soup.find_all('a', class_='product-title')
 
-        print(response)
+        for au in articles_info:
+            art_url = au.get('href')
+            articles_urls_list.append(art_url)
+
+        # time.sleep(randrange(2, 5))
+        print(f'We got {page}/{pagination_count}')
+        # print(response)
+
+    with open('articles_urls.txt', 'w') as file:
+        for url in articles_urls_list:
+            file.write(f'{url}\n')
 
 
+    return 'Working link'
    # print(pagination_count)
 
-def main():
-    get_article_urls(url='https://elmakon.uz/telefony-gadzhety-aksessuary/smartfony/')
 
+def get_data(file_path):
+    with open(file_path) as file:
+        urls_list = [line.strip() for line in file.readlines()]
+   
+    s = requests.Session()
+
+    for url in urls_list[:5]:
+        response = s.get(url=url, headers=header)
+        soup = BeautifulSoup(response.text, 'lxml')
+
+        article_title = soup.find('div', class_='ut2-pb ty-product-block ut2-three-columns ty-product-detail').find('h1', class_='ut2-pb__title').text.strip()
+        article_selling_info = soup.find('div', class_='ty-control-group product-list-field').text.strip()
+        prodact_info = soup.find('div', class_='ty-features-list').text.strip()
+
+
+        print(prodact_info)
+
+def main():
+    # print(get_article_urls(url='https://elmakon.uz/telefony-gadzhety-aksessuary/smartfony/'))
+    get_data('articles_urls.txt')
 
 if __name__ == '__main__':
     main()
